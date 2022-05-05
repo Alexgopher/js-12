8005d48a23a61dc8d59a1836f7997b7f037c60d2
Alexgopher <alex.eloter@gmail.com>
2022-05-05 14:43:19 +0300

    1
    Make homework

--- topics/about_numbers.js
+++ topics/about_numbers.js
@@ -5,12 +5,12 @@ test("types", function() {
     var typeOfIntegers = typeof(6);
     var typeOfFloats = typeof(3.14159);
     equal(true, typeOfIntegers === typeOfFloats, 'are ints and floats the same type?');
-    equal(, typeOfIntegers, 'what is the javascript numeric type?');
-    equal(__, 1.0, 'what is a integer number equivalent to 1.0?');
+    equal('number', typeOfIntegers, 'what is the javascript numeric type?');
+    equal(1, 1.0, 'what is a integer number equivalent to 1.0?');
 });
 
 test("NaN", function() {
     var resultOfFailedOperations = 7/'apple';
-    equal(__, isNaN(resultOfFailedOperations), 'what will satisfy the equals assertion?');
-    equal(__, resultOfFailedOperations == NaN, 'is NaN == NaN?');
+    equal(true, isNaN(resultOfFailedOperations), 'what will satisfy the equals assertion?');
+    equal(false, resultOfFailedOperations == NaN, 'is NaN == NaN?');
 });
